8c7daf9bdda1dacf428a688b77e6353bc32e0b4d
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-10 16:14:10 +0000

    Paramétrage de l'inversion de dépendance sur la couche Application et de l'injection de dépendance dans le container avec l'ajout de AddApplication

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplication();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
