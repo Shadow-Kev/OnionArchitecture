0c201066ef3f58923b556263f81b4311521691f7
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2023-07-11 19:44:52 +0000

    Configuration et implémentation de ProductController

---
+++ WebApi/Controllers/v1/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.ProductFeatures.Commands;
+using Application.Features.ProductFeatures.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.v1
+{
+    public class ProductController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllProductsQuery()));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetProductByIdQuery { Id = id }));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteProductByIdCommand command)
+        {
+            return Ok(await Mediator.Send(new DeleteProductByIdCommand { Id = command.Id }));
+        }
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+            return Ok(await Mediator.Send(command));
+        }
+
+    }
+}
